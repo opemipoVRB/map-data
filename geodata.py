6fc6a05d9df75c5655bf54c9cfead0311c7e49bf
opemipoVRB <opemipodurodola@gmail.com>
2020-06-18 04:00:46 +0100

    Initial commit

--- geodata.py
+++ geodata.py
@@ -5,7 +5,7 @@ import pycristoforo as pyc
 
 country = pyc.get_shape("Nigeria")
 
-points = pyc.geoloc_generation(country, 14005, "Nigeria")
+points = pyc.geoloc_generation(country, 140058, "Nigeria")
 
 with open('geodata.csv', mode='w') as employee_file:
     employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
